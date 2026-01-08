4ec5079f5ee6db69278afeee4d2f78a0d013ed61
adarshkushwaha <adarshkushwaha296@gmail.com>
2026-01-08 23:13:20 +0530

    pushed nested

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import MultiForm from './pages/MultiForm'
 import Home from './pages/Home'
 import OtpInput from './pages/OtpInput'
+import NestedCheckbox from './pages/NestedCheckbox'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/multi-form" element={<MultiForm />} />
         <Route path="/otp" element ={<OtpInput/>}/>
+        <Route path="/nested-checkbox" element ={<NestedCheckbox/>}/>
       </Routes>
     </>
   )
